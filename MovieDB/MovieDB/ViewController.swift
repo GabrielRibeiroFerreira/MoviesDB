32a4e31ada7fc551343912dfa9d907839d251bac
Gabriel Ferreira <ferreira.ribeirog@gmail.com>
2020-08-31 16:08:19 -0300

    project created and pod installed

---
+++ MovieDB/MovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Gabriel Ferreira on 31/08/20.
+//  Copyright © 2020 Ribeiro Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
